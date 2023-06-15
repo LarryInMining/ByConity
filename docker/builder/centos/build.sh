3a7ded640bfedbfa0345cfebe125894ca6c7eae2
Dao <minh.dao@bytedance.com>
2023-06-15 14:17:32 +0800

    add_centos

--- docker/builder/centos/build.sh
+++ docker/builder/centos/build.sh
@@ -4,7 +4,8 @@ set -e
 #ccache -s # uncomment to display CCache statistics
 mkdir -p /server/build_docker_centos
 cd /server/build_docker_centos
-cmake -G Ninja /server "-DCMAKE_C_COMPILER=$(command -v clang-11)" "-DCMAKE_CXX_COMPILER=$(command -v clang++-11)"
+#cmake -G Ninja /server "-DCMAKE_C_COMPILER=$(command -v clang-11)" "-DCMAKE_CXX_COMPILER=$(command -v clang++-11)"
+cmake -G Ninja /server "-DCMAKE_C_COMPILER=$(command -v clang)" "-DCMAKE_CXX_COMPILER=$(command -v clang++)"
 
 # Set the number of build jobs to the half of number of virtual CPU cores (rounded up).
 # By default, ninja use all virtual CPU cores, that leads to very high memory consumption without much improvement in build time.
